f88a971a5ed73051d09ea6f7706f1fe2a85e3354
wdh <786717306@qq.com>
2020-10-10 11:48:37 +0800

    1

--- mainlibrary/src/main/java/com/wdh/mainlibrary/ui/fragment/MainFragment.java
+++ mainlibrary/src/main/java/com/wdh/mainlibrary/ui/fragment/MainFragment.java
@@ -2,7 +2,6 @@ package com.wdh.mainlibrary.ui.fragment;
 
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +13,12 @@ import androidx.fragment.app.Fragment;
 import com.wdh.mainlibrary.control.MainFragCtrl;
 import com.wdh.mainlibrary.databinding.FragmentMainBinding;
 
-import java.util.logging.Logger;
-
 
 public class MainFragment extends Fragment {
+
+    private boolean isNavigationViewInit = false;//记录是否已经初始化过一次视图
+    private View lastView = null;//记录上次创建的view
+
 
     private MainFragCtrl mainFragCtrl;
 
@@ -25,15 +26,22 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //View mContentView = inflater.inflate(R.layout.fragment_main, container, false);
-        FragmentMainBinding binding = FragmentMainBinding.inflate(inflater, container, false);
-        mainFragCtrl = new MainFragCtrl(binding, this);
-        binding.setViewCtrl(mainFragCtrl);
-        return binding.getRoot();
+        //如果fragment的view已经创建则不再重新创建
+        if (lastView == null) {
+            FragmentMainBinding binding = FragmentMainBinding.inflate(inflater, container, false);
+            mainFragCtrl = new MainFragCtrl(binding, this);
+            binding.setViewCtrl(mainFragCtrl);
+            lastView = binding.getRoot();
+        }
+        return lastView;
     }
 
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        mainFragCtrl.setViewPage(this);
+        if (!isNavigationViewInit) {//初始化过视图则不再进行view和data初始化
+            mainFragCtrl.setViewPage(this);
+            isNavigationViewInit = true;
+        }
     }
 }
--- mainlibrary/src/main/java/com/wdh/mainlibrary/ui/fragment/MyFragment.java
+++ mainlibrary/src/main/java/com/wdh/mainlibrary/ui/fragment/MyFragment.java
@@ -9,16 +9,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.wdh.mainlibrary.R;
-import com.wdh.mainlibrary.databinding.FragmentMainItemBinding;
 import com.wdh.mainlibrary.databinding.FragmentMyBinding;
 
 public class MyFragment extends Fragment {
+
+    private View lastView = null;//记录上次创建的view
+    private boolean isNavigationViewInit = false;//记录是否已经初始化过一次视图
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        View mContentView = inflater.inflate(R.layout.fragment_my, container, false);
-        FragmentMyBinding binding=FragmentMyBinding.inflate(inflater,container,false);
-        return binding.getRoot();
+        if (lastView == null) {
+            FragmentMyBinding binding = FragmentMyBinding.inflate(inflater, container, false);
+            lastView = binding.getRoot();
+        }
+        return lastView;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        if (!isNavigationViewInit) {//初始化过视图则不再进行view和data初始化
+            super.onViewCreated(view, savedInstanceState);
+            isNavigationViewInit = true;
+        }
     }
 }
